dc7f86a47d61300b528167aad5ffb74959712f69
pepeg <hoanglong2205@gmail.com>
2021-09-09 13:50:27 +0900

    automate deploywith new instance

--- script/user-metadata.sh
+++ script/user-metadata.sh
@@ -1,9 +1,11 @@
 sudo yum install -y httpd
 sudo service httpd start
+sudo yum install -y ruby
 sudo yum install -y wget
 curl https://s3.dualstack.ap-northeast-1.amazonaws.com/aws-xray-assets.ap-northeast-1/xray-daemon/aws-xray-daemon-3.x.rpm -o /home/ec2-user/xray.rpm
 yum install -y /home/ec2-user/xray.rpm
-cd /home/ec2-user
+sudo -s
+cd /home/ec2-user/
 wget https://aws-codedeploy-ap-northeast-1.s3.amazonaws.com/latest/install
-chmod +x ./install
+sudo chmod +x ./install
 sudo ./install auto
